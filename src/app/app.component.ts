78374211535f6d8ec3c84c4c58a868849735732c
Tim van Elst <t.van.elst@spread-it.nl>
2017-06-10 22:18:12 +0200

    3. MASTER/DETAIL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,31 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-   title = 'Tour of Heroes';
+    heroes = HEROES;
+    selectedHero: Hero;
+    title = 'Tour of Heroes';
+
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
   }
