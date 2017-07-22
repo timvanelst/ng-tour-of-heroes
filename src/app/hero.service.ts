95bc2296abd50b38b8654b59549ad660826623df
Tim van Elst <t.van.elst@spread-it.nl>
2017-07-22 21:26:10 +0200

    implemented create, update and delete in components and service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -26,6 +26,33 @@ export class HeroService{
         .catch(this.handleError);
     }
 
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    create(name: string): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }    
+
+    delete(id: number): Promise<void> {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.delete(url, {headers: this.headers})
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
+
     getHeroesSlowly(): Promise<Hero[]> {
         return new Promise(resolve => 
         // Simulate server latency with 2 second delay
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -15,23 +15,42 @@ export class HeroesComponent implements OnInit {
     private heroService: HeroService,
     private router: Router) { }
 
-    heroes: Hero[];
-    selectedHero: Hero;
+  heroes: Hero[];
+  selectedHero: Hero;
 
-    ngOnInit() : void {
-      this.getHeroes();
-    }
-
-    getHeroes(): void {
-      this.heroService.getHeroes()
-        .then(heroes => this.heroes = heroes);
-    }
-
-    onSelect(hero: Hero): void {
-      this.selectedHero = hero;
-    }
-
-    gotoDetail(): void {
-      this.router.navigate(['/detail', this.selectedHero.id]);
-    }
+  ngOnInit() : void {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.heroes = this.heroes.filter(h => h !== hero);
+          if (this.selectedHero === hero) { this.selectedHero = null; }
+        });
+  }
+}
